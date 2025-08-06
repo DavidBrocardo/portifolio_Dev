fe5dce368c1fc20b08f759f801f75e06c9a91f63
davidbrocardo@gmail.com <70301267+DavidBrocardo@users.noreply.github.com>
2025-08-05 21:59:28 -0300

    Criado estutura básica do portifólio

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggle = document.getElementById('menu-toggle');
+  const nav = document.getElementById('menu-nav');
+
+  toggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+  });
