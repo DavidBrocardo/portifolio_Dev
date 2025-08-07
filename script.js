c1007cc07d9d20d8ece00b4c6d271d65f7b29fc7
davidbrocardo@gmail.com <70301267+DavidBrocardo@users.noreply.github.com>
2025-08-06 22:37:48 -0300

    Menu para celular

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const toggle = document.getElementById('menu-toggle');
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.getElementById('menu-toggle');
   const nav = document.getElementById('menu-nav');
 
-  toggle.addEventListener('click', () => {
-    nav.classList.toggle('active');
-  });
+  if (toggle && nav) {
+    toggle.addEventListener('click', () => {
+      nav.classList.toggle('active');
+    });
+  }
+});
